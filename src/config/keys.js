1e5b8b21d5e06d8d8b2f4843cdf0839fa0b96e0c
Kingusha23 <kingusha2333@gmail.com>
2019-04-29 20:10:33 +0300

    test

--- src/config/keys.js
+++ src/config/keys.js
@@ -1,5 +1,8 @@
-if (process.env.NODE_ENV === 'production') {
-  module.exports = require('./keys_prod');
-} else {
-  module.exports = require('./keys_dev');
-}
+// if (process.env.NODE_ENV === 'production') {
+//   module.exports = require('./keys_prod');
+// } else {
+//   module.exports = require('./keys_dev');
+// }
+module.exports = {
+  github: 'fbcab56ccb34c2813bc2662adf949477f8cc73ba'
+};
